1acf64dea9133a6c55c8757849b31d9a5274a440
Rui <rucah@sapo.pt>
2018-09-11 17:26:11 +0100

    Revert "Add modal component"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatDialogModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
-import { ModalComponent } from './modal/modal.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ModalComponent,
-  ],
-  entryComponents: [
-    ModalComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    MatDialogModule,
-    BrowserAnimationsModule
+    BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
